c64403e1bdd2a90c166aed2bbc2b9cd7e1246d6e
PasqualePaliotta <114742509+PasqualePaliotta@users.noreply.github.com>
2023-03-21 09:40:27 +0100

    Aggiunta file locali

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Title from "./components/Title";
+
+function App() {
+  return (
+    <div className="App">
+      <Title></Title>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function Title() {
+
+  return (
+    <div className="Title">
+        <h1>ciao</h1>
+    </div>
+  )
+}
+
+export default Title
